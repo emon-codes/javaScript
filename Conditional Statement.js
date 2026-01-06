c74061880b217ccd7a8a522769236e9e51b1aa92
EMON AHMED RANA <emon9996a@gmail.com>
2026-01-06 21:27:46 +0600

    Conditional Statement example

---
+++ Conditional Statement.js
@@ -0,0 +1,15 @@
+let point = 52;
+
+if (point >= 80) {
+  console.log("A+");
+}
+else if (point >= 60) {
+  console.log("B");
+}
+else if (point >= 50) {
+  console.log("C");
+}
+else {
+  console.log("F");
+}
+
