1fb015846b38bc861fb6f90acc490e9daf02669b
EMON AHMED RANA <emon9996a@gmail.com>
2026-01-23 20:50:06 +0600

    tarnary oprator

--- tarnary-porator.js
+++ tarnary-porator.js
@@ -10,3 +10,15 @@ let roll = Number(prompt("inter your Roll"))
 let result = roll === 1 ? "A+" : roll == 2 ? "B" : roll == 3 ? "C" : roll == 4 ? "D" : "F";
 
 console.log(result);
+
+
+
+
+let amount = 1200;
+
+let discount = amount >= 5000 ? "30% Discount"
+             : amount >= 3000 ? "20% Discount"
+             : amount >= 1000 ? "10% Discount"
+             : "No Discount";
+
+console.log(discount);
